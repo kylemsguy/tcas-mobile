e982d18a0add7dbba37ba9c32ddc0c5cdeb157f5
Kyle <kylezhou2002@hotmail.com>
2014-06-18 01:26:21 -0700

    Some formatting

--- TCaSMobile/src/com/kylemsguy/tcasmobile/tasks/LoginTask.java
+++ TCaSMobile/src/com/kylemsguy/tcasmobile/tasks/LoginTask.java
@@ -14,18 +14,17 @@ public class LoginTask extends AsyncTask<Object, Void, String> {
 		if (!(params[0] instanceof String) || !(params[1] instanceof String)
 				|| !(params[2] instanceof SessionManager))
 			return "Invalid Parameters";
-		
+
 		String username = (String) params[0];
 		String password = (String) params[1];
 		SessionManager sm = (SessionManager) params[2];
-		
+
 		try {
 			sm.login(username, password);
 		} catch (Exception e) {
 			return e.toString();
 		}
-			return "Login Success!";
+		return "Login Success!";
 	}
- 
 
 }
