f924aa5682273b7d55cf23604399c04763f38d01
Kyle Zhou <kylezhou2002@hotmail.com>
2014-09-29 13:38:12 -0400

    Started to revamp login screen

--- app/src/main/java/com/kylemsguy/tcasmobile/LoginActivity.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/LoginActivity.java
@@ -1,239 +1,342 @@
 package com.kylemsguy.tcasmobile;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import com.kylemsguy.tcasmobile.tasks.GetLoggedInTask;
-import com.kylemsguy.tcasmobile.tasks.LoginTask;
-import com.kylemsguy.tcasmobile.backend.SessionManager;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.ContentResolver;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build.VERSION;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends ActionBarActivity {
-	private static final String COOKIE_FILENAME = "cookies.txt";
-	private final boolean enableSaveCredentials = false;
+import java.util.ArrayList;
+import java.util.List;
 
-	private ConnectivityManager connMgr;
-	private SessionManager sm;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		setContentView(R.layout.activity_login);
+/**
+ * A login screen that offers login via email/password.
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+ */
+public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
-		sm = ((TCaSApp) getApplicationContext()).getSessionManager();
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
 
-		if (enableSaveCredentials) {
-			// Load cookies from file
-			try {
-				loadCookiesFromFile();
-			} catch (IOException e) {
-				System.out
-						.println("cookies.txt not found. There are no cookies to load.");
-			}
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
 
-			// Check if logged in
-			try {
-				if (new GetLoggedInTask().execute(sm).get()) {
-					Intent startMain = new Intent(this, AnswerActivity.class);
-					startActivity(startMain);
-					finish();
-				}
-			} catch (InterruptedException | ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-			// TODO cookiemanager code here
-			/*
-			 * CookieSyncManager.createInstance(this);
-			 * CookieSyncManager.getInstance().sync();
-			 * 
-			 * boolean loggedIn = false; try { loggedIn = new
-			 * GetLoggedInTask().execute(sm).get(); } catch
-			 * (InterruptedException | ExecutionException e) { // TODO
-			 * Auto-generated catch block e.printStackTrace(); }
-			 * 
-			 * if (loggedIn) { Intent intent = new Intent(this,
-			 * MainActivity.class); startActivity(intent); finish(); }
-			 */
-		}
-	}
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.username);
+        populateAutoComplete();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			Intent intent = new Intent(this, MainActivity.class);
-			startActivity(intent);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
 
-	private boolean checkLoggedIn() {
-		try {
-			return new GetLoggedInTask().execute(sm).get();
-		} catch (InterruptedException | ExecutionException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return false;
-		}
-	}
+    private void populateAutoComplete() {
+        if (VERSION.SDK_INT >= 14) {
+            // Use ContactsContract.Profile (API 14+)
+            getLoaderManager().initLoader(0, null, this);
+        } else if (VERSION.SDK_INT >= 8) {
+            // Use AccountManager (API 8+)
+            new SetupEmailAutoCompleteTask().execute(null, null);
+        }
+    }
 
-	private void loadCookiesFromFile() throws IOException {
-		FileInputStream fis;
-		StringBuilder sb = new StringBuilder();
 
-		fis = openFileInput(COOKIE_FILENAME);
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    public void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
 
-		int inputChar;
-		while ((inputChar = fis.read()) > -1) {
-			sb.append((char) inputChar);
-		}
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
 
-		System.out.println(sb.toString());
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
-		List<String> inputData = Arrays.asList(sb.toString().split("\n"));
+        boolean cancel = false;
+        View focusView = null;
 
-		sm.setCookies(inputData);
-	}
 
-	private void writeCookiesToFile() throws IOException {
-		FileOutputStream fos = openFileOutput(COOKIE_FILENAME,
-				Context.MODE_PRIVATE);
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
 
-		for (String cookie : sm.getCookies()) {
-			System.out.println(cookie);
-			fos.write(cookie.getBytes());
-		}
-		fos.close();
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
 
-	}
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
 
-	private boolean currNetworkConnected() {
-		NetworkInfo mobileNwInfo = connMgr
-				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-		NetworkInfo wifiNwInfo = connMgr
-				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
 
-		return ((mobileNwInfo == null ? false : mobileNwInfo.isConnected()) || 
-				(wifiNwInfo == null ? false	: wifiNwInfo.isConnected()));
-	}
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
 
-	private void showDialog(String message) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(message);
-		builder.setPositiveButton("OK", null);
-		builder.setCancelable(true);
-		AlertDialog dialog = builder.create();
-		dialog.show();
-	}
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-	public void loggestIn(View view) {
-		EditText user = (EditText) findViewById(R.id.login_username);
-		EditText pass = (EditText) findViewById(R.id.login_password);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
 
-		String username = user.getText().toString();
-		String password = pass.getText().toString();
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
 
-		// login
-		try {
-			new LoginTask().execute(username, password, sm).get();
-		} catch (InterruptedException | ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this,
+                // Retrieve data rows for the device user's 'profile' contact.
+                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
 
-		// check if logged in
-		boolean loggedIn = checkLoggedIn();
+                // Select only email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE +
+                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+                .CONTENT_ITEM_TYPE},
 
-		if (!loggedIn) {
-			if (currNetworkConnected())
-				showDialog("Login failed. Check your username or password.");
-			else
-				showDialog("Login failed. Check your internet connection.");
-			return;
-		} else {
-			if (enableSaveCredentials) {
-				try {
-					System.out.println("Saving login credentials...");
-					writeCookiesToFile();
-				} catch (IOException e) {
-					showDialog("Could not save login credentials.");
-					e.printStackTrace();
-				}
-			}
-		}
+                // Show primary email addresses first. Note that there won't be
+                // a primary email address if the user hasn't specified one.
+                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+    }
 
-		// start the new activity
-		// Intent intent = new Intent(this, AnswerActivity.class);
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
-		/*
-		 * // DEBUG: get new question AsyncTask<SessionManager, Void, String>
-		 * getQuestionTask = new GetQuestionTask() .execute(sm);
-		 * 
-		 * String result; try { result = getQuestionTask.get(); } catch
-		 * (InterruptedException | ExecutionException e) { result =
-		 * e.toString(); }
-		 * 
-		 * // DEBUG: Display whatever the result is as a toast
-		 * Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG)
-		 * .show();
-		 */
-		finish();
-	}
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
+        }
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+        addEmailsToAutoComplete(emails);
+    }
 
-		public PlaceholderFragment() {
-		}
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_login,
-					container, false);
-			return rootView;
-		}
-	}
+    }
 
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+    /**
+     * Use an AsyncTask to fetch the user's email addresses on a background thread, and update
+     * the email text field with results on the main UI thread.
+     */
+    class SetupEmailAutoCompleteTask extends AsyncTask<Void, Void, List<String>> {
+
+        @Override
+        protected List<String> doInBackground(Void... voids) {
+            ArrayList<String> emailAddressCollection = new ArrayList<String>();
+
+            // Get all emails from the user's contacts and copy them to a list.
+            ContentResolver cr = getContentResolver();
+            Cursor emailCur = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                    null, null, null);
+            while (emailCur.moveToNext()) {
+                String email = emailCur.getString(emailCur.getColumnIndex(ContactsContract
+                        .CommonDataKinds.Email.DATA));
+                emailAddressCollection.add(email);
+            }
+            emailCur.close();
+
+            return emailAddressCollection;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> emailAddressCollection) {
+            addEmailsToAutoComplete(emailAddressCollection);
+        }
+    }
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mEmailView.setAdapter(adapter);
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                finish();
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
 }
+
+
+
