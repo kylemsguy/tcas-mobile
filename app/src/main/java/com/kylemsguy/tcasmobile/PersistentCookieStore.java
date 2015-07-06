5b935ee5842ff729d5a77b8684950edc873a166f
Kyle Zhou <kylezhou2002@hotmail.com>
2015-07-06 11:03:06 -0700

    Added relevant docstrings

--- app/src/main/java/com/kylemsguy/tcasmobile/PersistentCookieStore.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/PersistentCookieStore.java
@@ -21,12 +21,30 @@ public class PersistentCookieStore implements CookieStore {
             cookies = new TreeMap<>();
     }
 
+    /**
+     * Adds one HTTP cookie to the store. This is called for every incoming HTTP response.
+     * A cookie to store may or may not be associated with an URI. If it is not associated with an URI,
+     * the cookie's domain and path attribute will indicate where it comes from. If it is associated with an URI
+     * and its domain and path attribute are not speicifed, given URI will indicate where this cookie comes from.
+     * If a cookie corresponding to the given URI already exists, then it is replaced with the new one
+     *
+     * @param uri    the uri this cookie associated with. if null, this cookie will not be associated with an URI
+     * @param cookie The cookie to store
+     */
     @Override
     public void add(URI uri, HttpCookie cookie) {
         if (cookie == null)
             throw new NullPointerException();
         // TODO commit cookie store to SharedPreferences
     }
+
+    /**
+     * Retrieve cookies associated with given URI, or whose domain matches the given URI.
+     * Only cookies that have not expired are returned. This is called for every outgoing HTTP request.
+     *
+     * @param uri The URI
+     * @return an immutable list of HttpCookie, return empty list if no cookies match the given URI
+     */
 
     @Override
     public List<HttpCookie> get(URI uri) {
@@ -36,16 +54,31 @@ public class PersistentCookieStore implements CookieStore {
         return null;
     }
 
+    /**
+     * Get all not-expired cookies in cookie store.
+     * @return an immutable list of http cookies; return empty list if there's no http cookie in store
+     */
     @Override
     public List<HttpCookie> getCookies() {
         return null;
     }
 
+    /**
+     * Get all URIs which identify the cookies in this cookie store.
+     * @return an immutable list of URIs; return empty list if no cookie in this cookie store is associated with an URI
+     */
     @Override
     public List<URI> getURIs() {
         return null;
     }
 
+    /**
+     * Remove cookie from store
+     *
+     * @param uri the uri this cookie associated with. if null, the cookie to be removed is not associated with an URI when added; if not null, the cookie to be removed is associated with the given URI when added.
+     * @param cookie The cookie to remove
+     * @return true if this store contained the specified cookie
+     */
     @Override
     public boolean remove(URI uri, HttpCookie cookie) {
         // return true; if successfully stored
@@ -54,6 +87,11 @@ public class PersistentCookieStore implements CookieStore {
         return false;
     }
 
+    /**
+     * Remove all cookies in this cookie store.
+     *
+     * @return true if this store changed as a result of the call
+     */
     @Override
     public boolean removeAll() {
         // return true; if successfully stored
