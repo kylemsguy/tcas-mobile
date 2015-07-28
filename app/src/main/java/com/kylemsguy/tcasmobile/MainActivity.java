1612de9a8920463a74b6b9c9fae4b6c7e8d111ab
Kyle Zhou <kylezhou2002@hotmail.com>
2015-07-27 20:37:35 -0700

    Pruned some useless imports

--- app/src/main/java/com/kylemsguy/tcasmobile/MainActivity.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/MainActivity.java
@@ -1,32 +1,20 @@
 package com.kylemsguy.tcasmobile;
 
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import com.kylemsguy.tcasmobile.tasks.GetLoggedInTask;
 import com.kylemsguy.tcasmobile.tasks.LogoutTask;
-import com.kylemsguy.tcasmobile.backend.AnswerManager;
-import com.kylemsguy.tcasmobile.backend.Question;
-import com.kylemsguy.tcasmobile.backend.QuestionManager;
 import com.kylemsguy.tcasmobile.backend.SessionManager;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -34,9 +22,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
