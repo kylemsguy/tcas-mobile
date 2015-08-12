bed84a1342e592e4795009351a677d4ca729019a
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-12 16:48:55 -0700

    MessageThread now implements Comparable

--- app/src/main/java/com/kylemsguy/tcasmobile/backend/MessageManager.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/backend/MessageManager.java
@@ -1,16 +1,41 @@
 package com.kylemsguy.tcasmobile.backend;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MessageManager {
 	private final String MESSAGES_URL = SessionManager.BASE_URL + "";
 
     private SessionManager sm;
 
+    private List<MessageThread> threads;
+
     public MessageManager(SessionManager sm) {
         this.sm = sm;
+        threads = new ArrayList<>();
     }
 
-    public void getMessageList() {
+    /**
+     * Returns a shallow copy of the thread list
+     *
+     * @return A shallow copy of the thread list
+     */
+    public List<MessageThread> getThreadList() {
+        return threads;
+    }
 
+    /**
+     * Returns an immutable list containing all the message threads.
+     *
+     * @return Immutable list of message threads
+     */
+    public List<MessageThread> getThreads() {
+        return Collections.unmodifiableList(threads);
+    }
+
+    public void addThread(MessageThread thread) {
+        threads.add(thread);
     }
 
 }
--- app/src/main/java/com/kylemsguy/tcasmobile/backend/MessageThread.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/backend/MessageThread.java
@@ -1,5 +1,7 @@
 package com.kylemsguy.tcasmobile.backend;
 
+import android.support.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.List;
  * Created by kyle on 12/08/15.
  *
  */
-public class MessageThread extends TCaSObject {
+public class MessageThread extends TCaSObject implements Comparable<MessageThread> {
     private String title;
     private Message lastMessage;
     private List<String> users;
@@ -59,6 +61,10 @@ public class MessageThread extends TCaSObject {
         return Collections.unmodifiableList(users);
     }
 
+    public Message getLastMessage() {
+        return lastMessage;
+    }
+
     @Deprecated
     public void addMessage(String sentBy, String message, double daysOffset) throws UserNotInConversationException {
         long unix = OhareanCalendar.daysOffsetToUnix(daysOffset);
@@ -86,6 +92,16 @@ public class MessageThread extends TCaSObject {
     @Override
     public String getContent() {
         throw new UnsupportedOperationException("Unsupported operation on a MessageThread.");
+    }
+
+    @Override
+    public int compareTo(@NonNull MessageThread o) {
+        if (lastMessage.getTimeReceived() < o.getLastMessage().getTimeReceived())
+            return -1;
+        else if (lastMessage.getTimeReceived() == o.getLastMessage().getTimeReceived())
+            return 0;
+        else
+            return 1;
     }
 
     public static class UserNotInConversationException extends Exception {
