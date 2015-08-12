00cfb79a6b9680a08d7c034e6dcc82c6316c02ac
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-12 16:41:22 -0700

    Finished implementation of Message and MessageThread

--- app/src/main/java/com/kylemsguy/tcasmobile/backend/Message.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/backend/Message.java
@@ -1,19 +1,41 @@
 package com.kylemsguy.tcasmobile.backend;
 
+import android.support.annotation.NonNull;
+
 /**
  * Created by kyle on 12/08/15.
+ * Message class for storing individual messages
  */
-public class Message extends TCaSObject {
+public class Message extends TCaSObject implements Comparable<Message> {
+    private String sentBy;
     private long timeReceived;
 
     @Deprecated
-    public Message(int id, String content, double dayOffset) {
-        super(id, content);
-        timeReceived = OhareanCalendar.daysOffsetToUnix(dayOffset);
+    public Message(int id, String sentBy, String content, double dayOffset) {
+        this(id, sentBy, content, OhareanCalendar.daysOffsetToUnix(dayOffset));
     }
 
-    public Message(int id, String content, long timeReceived) {
+    public Message(int id, String sentBy, String content, long timeReceived) {
         super(id, content);
+        this.sentBy = sentBy;
         this.timeReceived = timeReceived;
+    }
+
+    public long getTimeReceived() {
+        return timeReceived;
+    }
+
+    public String getSender() {
+        return sentBy;
+    }
+
+    @Override
+    public int compareTo(@NonNull Message o) {
+        if (timeReceived < o.getTimeReceived())
+            return -1;
+        else if (timeReceived == o.getTimeReceived())
+            return 0;
+        else
+            return 1;
     }
 }
--- app/src/main/java/com/kylemsguy/tcasmobile/backend/MessageThread.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/backend/MessageThread.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 /**
  * Created by kyle on 12/08/15.
+ *
  */
 public class MessageThread extends TCaSObject {
     private String title;
-    private long receivedTime;
+    private Message lastMessage;
     private List<String> users;
     private List<Message> messages;
 
@@ -17,10 +18,27 @@ public class MessageThread extends TCaSObject {
      * Creates a new MessageThread.
      *
      * @param id the ID of the thread
+     * @param title The title of the conversation
+     * @param users The name of the users
      */
-    public MessageThread(int id) {
+    public MessageThread(int id, String title, List<String> users, Message firstMessage) {
         super(id, null);
+
+        this.title = title;
+        this.users = new ArrayList<>();
+
+        // copy users into internal list
+        for (String user : users) {
+            this.users.add(user);
+        }
+
         messages = new ArrayList<>();
+        messages.add(firstMessage);
+        lastMessage = firstMessage;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     /**
@@ -32,13 +50,81 @@ public class MessageThread extends TCaSObject {
         return Collections.unmodifiableList(messages);
     }
 
-    public void addMessage(String message, long when) {
+    /**
+     * Returns an immutable list of the users in this conversation
+     *
+     * @return An immutable list of users
+     */
+    public List<String> getUsers() {
+        return Collections.unmodifiableList(users);
+    }
 
+    @Deprecated
+    public void addMessage(String sentBy, String message, double daysOffset) throws UserNotInConversationException {
+        long unix = OhareanCalendar.daysOffsetToUnix(daysOffset);
+        addMessage(sentBy, message, unix);
+    }
+
+    public void addMessage(String sentBy, String message, long when) throws UserNotInConversationException {
+        Message m = new Message(getId(), sentBy, message, when);
+        addMessage(m);
+    }
+
+    public void addMessage(Message message) throws UserNotInConversationException {
+        if (!users.contains(message.getSender()))
+            throw new UserNotInConversationException("User " + message.getSender() + " is not in conversation with ID " + getId());
+        messages.add(message);
+
+        if (lastMessage.compareTo(message) > 0) {
+            // resort because message is out of place
+            Collections.sort(messages);
+        } else {
+            lastMessage = message;
+        }
     }
 
     @Override
     public String getContent() {
         throw new UnsupportedOperationException("Unsupported operation on a MessageThread.");
+    }
+
+    public static class UserNotInConversationException extends Exception {
+        /**
+         * Constructs a new {@code Exception} that includes the current stack trace.
+         */
+        public UserNotInConversationException() {
+        }
+
+        /**
+         * Constructs a new {@code Exception} with the current stack trace and the
+         * specified detail message.
+         *
+         * @param detailMessage the detail message for this exception.
+         */
+        public UserNotInConversationException(String detailMessage) {
+            super(detailMessage);
+        }
+
+        /**
+         * Constructs a new {@code Exception} with the current stack trace, the
+         * specified detail message and the specified cause.
+         *
+         * @param detailMessage the detail message for this exception.
+         * @param throwable
+         */
+        public UserNotInConversationException(String detailMessage, Throwable throwable) {
+            super(detailMessage, throwable);
+        }
+
+        /**
+         * Constructs a new {@code Exception} with the current stack trace and the
+         * specified cause.
+         *
+         * @param throwable the cause of this exception.
+         */
+        public UserNotInConversationException(Throwable throwable) {
+            super(throwable);
+        }
     }
 
 }
