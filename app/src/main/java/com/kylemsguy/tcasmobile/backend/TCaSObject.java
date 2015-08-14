98f7c93a349d5cfca0dde4387087cce66a12012f
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-14 15:40:45 -0700

    Moved NoSuchQuestionException to TCaSObject

--- app/src/main/java/com/kylemsguy/tcasmobile/backend/TCaSObject.java
+++ app/src/main/java/com/kylemsguy/tcasmobile/backend/TCaSObject.java
@@ -113,4 +113,28 @@ public abstract class TCaSObject {
 
     }
 
+    public static class NoSuchQuestionException extends Exception {
+
+        public NoSuchQuestionException() {
+            // TODO Auto-generated constructor stub
+        }
+
+        public NoSuchQuestionException(String message) {
+            super(message);
+            // TODO Auto-generated constructor stub
+        }
+
+        public NoSuchQuestionException(Throwable cause) {
+            super(cause);
+            // TODO Auto-generated constructor stub
+        }
+
+        public NoSuchQuestionException(String message, Throwable cause) {
+            super(message, cause);
+            // TODO Auto-generated constructor stub
+        }
+
+    }
+
+
 }
